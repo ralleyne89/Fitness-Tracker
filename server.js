bea34b989d0a99fafd5b0b9aa6856fd21f51561d
Reggie Alleyne <reggiealleyne89@gmail.com>
2019-12-16 18:41:13 -0800

    adding handlebars folder and files

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ let db = require("./models");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(app.router)
 // Static directory to be served
 app.use(express.static("public"));
 
