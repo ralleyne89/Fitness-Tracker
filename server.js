33a01ee3d3fe8884ca04251c2bec674670d031f3
Reggie Alleyne <reggiealleyne89@gmail.com>
2019-12-16 18:28:59 -0800

    creating files and folder str

---
+++ server.js
@@ -0,0 +1,27 @@
+// require node packages
+const express = require('express');
+const path = require('path');
+const http = requre('http');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 5150;
+let db = require("./models");
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Static directory to be served
+app.use(express.static("public"));
+
+
+// api & html routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// begin server listening
+app.listen(PORT, () => console.log('Server starting on Port: ' + PORT));
